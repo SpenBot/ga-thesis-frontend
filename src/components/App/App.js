0dd890aa163481d065e7984973ead4d5147f44e9
Spenser Holstein <spenser.holstein@gmail.com>
2017-10-22 18:51:41 -0400

    Well, the GA thing works with no db

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import openSocket from 'socket.io-client'
+import './App.css'
+
+const socket = openSocket('http://localhost:4000')
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+    this.submitMessage = this.submitMessage.bind(this)
+  }
+
+  componentDidMount () {
+    socket.on('chat message', (msg) => {
+      this.setState({messages: this.state.messages.concat(msg)})
+    })
+  }
+
+  submitMessage (e) {
+    e.preventDefault()
+    let msg = document.getElementById('message').value
+    socket.emit('chat message', msg)
+    document.getElementById('message').value = ''
+  }
+
+  render () {
+    let messages = this.state.messages.map((message) => (<li>{message}</li>))
+    return (
+      <div>
+        <ul id='messages'>{messages}</ul>
+        <form action='' onSubmit={this.submitMessage}>
+          <input id='message' /><button>Send</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
